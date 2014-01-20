76a391db5fac26eaf69251006fd79b510b28f32e
Andrew Anderson <andrewloveskitties@gmail.com>
2014-01-20 08:00:47 -0800

    Add include guard to brick.hpp

--- src/entity/brick.hpp
+++ src/entity/brick.hpp
@@ -1,3 +1,6 @@
+#ifndef BRICK_HPP
+#define BRICK_HPP
+
 #include "../component_type.hpp"
 #include "../component/position.hpp"
 #include "../component/size.hpp"
@@ -8,3 +11,5 @@ class Brick : public Entity {
     public:
         Brick(int32_t x = 0, int32_t y = 0);
 };
+
+#endif // BRICK_HPP
